4e27858111074c734ee06698b490b0e5506a9360
Piero Urgiles <piero.urgiles@gmail.com>
2024-04-12 19:01:29 -0500

    Se agrega python.py al repo

---
+++ python/script_python.py
@@ -0,0 +1,23 @@
+import os
+
+location = "D:/Certus/Dataops/python/datasets"
+
+###Validar que la carpeta exista###
+if not os.path.exists(location):
+    ##En caso mi carpeta no exista, voy a crear una nueva##
+    os.mkdir(location) ##mkdir -> make directory
+else:
+    ##Si la carpeta ya existe, entonces borramos el contenido##
+    for root, dirs, files in os.walk(location, topdown=False):
+        for name in files:
+            os.remove(os.path.join(root,name)) ##elimino todos los archivos
+        for name in dirs:
+            os.rmdir(os.path.join(root,name)) ##rmdir -> remove directory / elimino todas mis carpetas
+            
+from kaggle.api.kaggle_api_extended import KaggleApi
+api = KaggleApi()
+api.authenticate()
+print(api.dataset_list(search='henryshan'))
+
+api.dataset_download_file('henryshan/starbucks','starbucks.csv',path=location,quiet=False)
+
